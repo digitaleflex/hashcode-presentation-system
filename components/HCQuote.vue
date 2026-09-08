5b9f41b3290c91448d3625df6c69abac58fba2da
Eflex <eflexcloud@gmail.com>
2026-09-08 01:47:25 +0100

    feat(design-system): add HashCode Slidev components and layouts

---
+++ components/HCQuote.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  quote: { type: String, required: true },
+  kicker: { type: String, default: 'KEY IDEA' }
+})
+</script>
+
+<template>
+  <div class="hc-quote">
+    <div class="hc-eyebrow">{{ kicker }}</div>
+    <blockquote>“{{ quote }}”</blockquote>
+    <div class="hc-quote-line"></div>
+  </div>
+</template>
