5733dc69a899f9f8fed0ebcd72cbb9666f552b74
Eflex <eflexcloud@gmail.com>
2026-09-08 01:47:22 +0100

    feat(design-system): add HashCode Slidev components and layouts

---
+++ components/HCMetric.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  value: { type: String, required: true },
+  label: { type: String, required: true }
+})
+</script>
+
+<template>
+  <div class="hc-metric">
+    <strong>{{ value }}</strong>
+    <span>{{ label }}</span>
+  </div>
+</template>
