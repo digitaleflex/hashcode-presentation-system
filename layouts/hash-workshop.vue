cc7bf8bd207ff839989b8586b8b55be95136f81d
Eflex <eflexcloud@gmail.com>
2026-09-08 01:47:35 +0100

    feat(design-system): add HashCode Slidev components and layouts

---
+++ layouts/hash-workshop.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="hc-layout hc-workshop-layout">
+    <div class="hc-workshop-tag">HANDS-ON</div>
+    <div class="hc-workshop-content"><slot /></div>
+    <div class="hc-workshop-footer">BUILD · TEST · SHARE</div>
+  </div>
+</template>
