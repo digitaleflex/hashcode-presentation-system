3a41d96fdff84042936e4d31e110f84c2b5603c2
Eflex <eflexcloud@gmail.com>
2026-09-08 01:47:33 +0100

    feat(design-system): add HashCode Slidev components and layouts

---
+++ layouts/hash-content.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="hc-layout hc-content-layout">
+    <div class="hc-content-header"><HCBrand compact /></div>
+    <main><slot /></main>
+    <div class="hc-page-line"></div>
+  </div>
+</template>
